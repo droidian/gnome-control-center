84f2147bca0b88f10f9287e55bd3c71953c2f978
Jeremy Bicha <jeremy.bicha@canonical.com>
2022-08-22 08:31:15 -0400

    firmware-security: Disable
    Not yet useful enough to justify its inclusion by default here
    
    You can get the same info by running
    fwupdmgr security
    
    If you drop this patch, add a Depends: fwupd [linux-any]
    
    https://launchpad.net/bugs/1987162
    
    https://gitlab.gnome.org/GNOME/gnome-control-center/-/issues/1938

--- shell/cc-panel-loader.c
+++ shell/cc-panel-loader.c
@@ -113,7 +113,7 @@ static CcPanelLoaderVtable default_panels[] =
   PANEL_TYPE("info-overview",    cc_info_overview_panel_get_type,        NULL),
   PANEL_TYPE("keyboard",         cc_keyboard_panel_get_type,             NULL),
   PANEL_TYPE("location",         cc_location_panel_get_type,             NULL),
-  PANEL_TYPE("firmware-security",cc_firmware_security_panel_get_type,    cc_firmware_security_panel_static_init_func),
+  // PANEL_TYPE("firmware-security",cc_firmware_security_panel_get_type,    cc_firmware_security_panel_static_init_func),
   PANEL_TYPE("microphone",       cc_microphone_panel_get_type,           NULL),
   PANEL_TYPE("mouse",            cc_mouse_panel_get_type,                NULL),
   PANEL_TYPE("multitasking",     cc_multitasking_panel_get_type,         NULL),
