e4e289c872be18c228d5af5fe522ffb6428eeeb3
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2023-07-07 21:57:38 -0400

    panels: add usb panel
    The following commits (not relevant anymore,
    as the panel has been rewritten) have been squashed:
    
    panels: add mtp panel (2976ca6beeb536f3f4bbae708ee787f73e161153)
    mtp: allow usb state switching (921f79cc06e85627aa067b88f83a1f094ea8fcb7)
    mtp: adapt to 45.rc (40061761fe77ea729e4533678abe373d9b673d90)
    mtp: fix the typo of mtp enabled switch (4986a30bfc2410278f18ad9c9d8521b011cef3fc)
    panels: Rename mtp panel to usb panel (82c3016cb5d0a8a61f508f23441d723796d1758d)
    
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- shell/main-droidian.c
+++ shell/main-droidian.c
@@ -80,6 +80,7 @@ extern void cc_wwan_panel_static_init_func (void);
 
 extern GType cc_droidian_encryption_panel_get_type (void);
 extern GType cc_fingerprint_panel_get_type (void);
+extern GType cc_usb_panel_get_type (void);
 
 #define PANEL_TYPE(name, get_type, init_func) { name, get_type, init_func }
 
@@ -113,6 +114,7 @@ static CcPanelLoaderVtable droidian_panels[] =
 #endif
   PANEL_TYPE("droidian-encryption", cc_droidian_encryption_panel_get_type, NULL),
   PANEL_TYPE("fingerprint", cc_fingerprint_panel_get_type, NULL),
+  PANEL_TYPE("usb",              cc_usb_panel_get_type,                  NULL),
 };
 
 
@@ -143,6 +145,7 @@ static gchar * const droidian_panel_order[] = {
   "keyboard",
   "printers",
   "wacom",
+  "usb",
 
   "separator",
 
