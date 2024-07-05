9db97af1a34a9f1071fe69e403107b3c02386c26
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-07-06 00:22:59 +0200

    panels: add fingerprint panel
    The following commit has been squashed:
    
    panels: fingerprint: Do not crash if service unavailable (a9ba92610d5f4d8eabee27cb5ecb447a86492126)

---
+++ panels/fingerprint/cc-fingerprint-panel.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2024 Cédric Bellegarde <cedric.bellegarde@adishatz.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <adwaita.h>
+#include <shell/cc-panel.h>
+
+G_BEGIN_DECLS
+
+#define CC_TYPE_FINGERPRINT_PANEL (cc_fingerprint_panel_get_type ())
+G_DECLARE_FINAL_TYPE (CcFingerprintPanel, cc_fingerprint_panel, CC, FINGERPRINT_PANEL, CcPanel)
+
+CcFingerprintPanel *cc_fingerprint_panel_new (void);
+
+G_END_DECLS
